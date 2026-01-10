49aee49be1265280c69635ae9e8c88b9005928e7
draganovdimitar2 <draganovdimitar2@gmail.com>
2026-01-10 16:25:07 +0200

    feat: add home_page app

---
+++ home_page/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+# Create your views here.
+def home_page(request: HttpRequest) -> HttpResponse:
+    return render(request, 'index.html')
