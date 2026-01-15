7f8f3ae85ce9b61a0d88ab4e8235a563ff36ec71
draganovdimitar2 <draganovdimitar2@gmail.com>
2026-01-15 17:55:46 +0200

    feat(models): add AgeCategories and Disciplines models

--- athletes/models.py
+++ athletes/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.db.models import Model
 
 
 # Create your models here.
-class Athletes(models.Model):
-    class Gender(models.TextChoices):
-        MALE = 'M', 'Male'
-        FEMALE = 'F', 'Female'
 
+class GenderChoices(models.TextChoices):
+    MALE = 'M', 'Male'
+    FEMALE = 'F', 'Female'
+
+
+class Athletes(models.Model):
     first_name = models.CharField(
         max_length=50
     )
@@ -22,7 +25,11 @@ class Athletes(models.Model):
     )
     gender = models.CharField(
         max_length=1,
-        choices=Gender.choices
+        choices=GenderChoices.choices
+    )
+    disciplines = models.ManyToManyField( # many to many relation with discipline table
+        'Discipline',
+        related_name='athletes'
     )
     created_at = models.DateTimeField(
         auto_now_add=True
@@ -36,3 +43,76 @@ class Athletes(models.Model):
 
     class Meta:
         ordering = ['last_name', 'first_name']
+
+
+class AgeCategories(models.Model):
+    class Name(models.TextChoices):
+        UNDER_14 = 'U14', 'Under 14'
+        UNDER_16 = 'U16', 'Under 16'
+        UNDER_18 = 'U18', 'Under 18'
+        UNDER_20 = 'U20', 'Under 20'
+        UNDER_23 = 'U23', 'Under 23'
+        SENIOR_OPEN = 'SEN', 'Senior / Open'
+        VETERANS_35 = 'V35', 'Veterans 35–39'
+        VETERANS_40 = 'V40', 'Veterans 40–44'
+        VETERANS_45 = 'V45', 'Veterans 45–49'
+        VETERANS_50 = 'V50', 'Veterans 50–54'
+        VETERANS_55 = 'V55', 'Veterans 55–59'
+        VETERANS_60 = 'V60', 'Veterans 60+'
+
+    name = models.CharField(
+        max_length=4,
+        choices=Name.choices,
+        unique=True  # have to save each category just one time in db to make relations work as intended
+    )
+
+    gender = models.CharField(
+        max_length=1,
+        choices=GenderChoices.choices
+    )
+
+    min_age = models.PositiveIntegerField(
+        blank=True,
+        null=True
+    )
+
+    max_age = models.PositiveIntegerField(
+        blank=True,
+        null=True
+    )
+
+    def save(self, *args, **kwargs):
+        category_age_map = {
+            'U14': (12, 14),
+            'U16': (14, 16),
+            'U18': (16, 18),
+            'U20': (18, 20),
+            'U23': (20, 23),
+            'SEN': (20, None),  # no upper limit for senior
+            'V35': (35, 39),
+            'V40': (40, 44),
+            'V45': (45, 49),
+            'V50': (50, 54),
+            'V55': (55, 59),
+            'V60': (60, None),  # no upper limit
+        }
+
+        if self.name in category_age_map:
+            self.min_age, self.max_age = category_age_map[
+                self.name]  # unpack the tuple and set min and max age based on category name
+
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.get_name_display()} ({self.get_gender_display()})"
+
+
+class Discipline(models.Model):
+    name = models.CharField(
+        blank=False,
+        null=False,
+        unique=True
+    )
+
+    def __str__(self) -> str:
+        return self.name
